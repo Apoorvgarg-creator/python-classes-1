f65da6bb39fc670cd9064f3a65c90ef19e7c0bc6
Apoorvgarg-Creator <57873504+Apoorvgarg-creator@users.noreply.github.com>
2024-09-22 08:37:13 +0530

    New: File Operaiton

---
+++ file_ops.py
@@ -0,0 +1,32 @@
+# Basic Operation on Files using Python !
+
+import os
+# f = open("test2.txt",'a')
+
+# # print(f)
+
+# # file mode -
+# # 1. 'r' - read - 
+# # 2. 'w' - write -
+# # 3. 'a' - append -
+# # 4. 'x' - create -
+
+
+# # read
+# # print(f.read())
+
+# # # Read some part of it 
+# # print(f.read(24))
+
+# # Write operations
+# f.write("Adding new line !")
+
+# Create a new file ?
+
+# f = open("testFileCreation.txt","w")
+# f.write("This is test file Creation")
+# f.close()
+
+
+# Delete a file
+os.remove("testFileCreation.txt")
